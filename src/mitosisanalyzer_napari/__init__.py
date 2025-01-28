e6df8ae8e2ea2a67917c4b9a042ec96e18ddee00
khs3z <khs3z@virginia.edu>
2025-01-28 13:46:21 -0500

    reorganized package to adhere to napari plugin convenstions

--- src/mitosisanalyzer_napari/__init__.py
+++ src/mitosisanalyzer_napari/__init__.py
@@ -1,3 +1,13 @@
+try:
+    from ._version import version as __version__
+except ImportError:
+    __version__ = "unknown"
+from ._widget import (
+    TrackEditorWidget,
+)
+
+__all__ = ("TrackEditorWidget",)
+
 from napari.utils.notifications import show_info
 
 
